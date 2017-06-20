3473912819968448c1381ae6e420c1158f943e34
Sumate <sumate@angstromsolutions.net>
2017-06-20 09:12:36 +0700

    change parameter name

--- src/app/comma-input/comma-input.component.ts
+++ src/app/comma-input/comma-input.component.ts
@@ -7,72 +7,40 @@ import {Input} from "@angular/core";
 })
 
 export class CommaInputComponent{
-    @Input() value : string = "";
+    @Input() inputValue : string = "";
     valueChange(): void{
-        let valNoComma = this.value.replace(/,/g ,"");
+        let valNoComma = this.inputValue.replace(/,/g ,"");
         if(isNaN(Number(valNoComma)) && valNoComma != "-"){
-            this.value = "";
+            this.inputValue = "";
             return; 
         }
         let minusIndex : number = valNoComma.indexOf("-");
-        var num : string;
+        var valOnlyNumber : string;
         if(minusIndex == 0){
-            num = valNoComma.substring(1,valNoComma.length);
+            valOnlyNumber = valNoComma.substring(1,valNoComma.length);
         }else{
-            num = valNoComma;
+            valOnlyNumber = valNoComma;
         }
-        this.value = "";
+        this.inputValue = "";
     
-        var s = "";
-        for(var _i = num.length; _i-3 >= 0; _i = _i-3){
-                s = "," + num.substring(_i-3, _i) + s;
+        var returnValue = "";
+        for(var _i = valOnlyNumber.length; _i-3 >= 0; _i = _i-3){
+                returnValue = "," + valOnlyNumber.substring(_i-3, _i) + returnValue;
         }
-        let exceedNumber = num.length % 3;
+        let exceedNumber = valOnlyNumber.length % 3;
         if(exceedNumber == 0){
-            s = s.substring(1, s.length);
+            returnValue = returnValue.substring(1, returnValue.length);
         }
         else{
-            s = num.substring(0, exceedNumber) + s;
+            returnValue = valOnlyNumber.substring(0, exceedNumber) + returnValue;
         }
-        while(s.length > 1 && (s[0] == "0" || s[0] == ",")){
-            s = s.substring(1, s.length); console.log("55 : "+s);
-        }
-        if(minusIndex == 0){
-            s = "-" + s;
-        }
-        this.value += s;
-        return;
-    }
-    blurChange() : void{
-        let valNoComma = this.value.replace(/,/g ,"");
-        console.log("new" + valNoComma);
-        let minusIndex : number = valNoComma.indexOf("-");
-        var num : string;
-        if(minusIndex == 0){
-            num = valNoComma.substring(1,valNoComma.length);
-        }else{
-            num = valNoComma;
-        }
-        this.value = "";
-    
-        var s = "";
-        for(var _i = num.length; _i-3 >= 0; _i = _i-3){
-                s = "," + num.substring(_i-3, _i) + s;
-        }
-        let exceedNumber = num.length % 3;
-        if(exceedNumber == 0){
-            s = s.substring(1, s.length);
-        }
-        else{
-            s = num.substring(0, exceedNumber) + s;
-        }
-        while(s.length > 1 && (s[0] == "0" || s[0] == ",")){
-            s = s.substring(1, s.length); console.log("55 : "+s);
+        while(returnValue.length > 1 && (returnValue[0] == "0" || returnValue[0] == ",")){
+            returnValue = returnValue.substring(1, returnValue.length); console.log("55 : "+ returnValue);
         }
         if(minusIndex == 0){
-            s = "-" + s;
+            returnValue = "-" + returnValue;
         }
-        this.value += s;
+        this.inputValue += returnValue;
         return;
     }
 }
